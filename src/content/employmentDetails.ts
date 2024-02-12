68af3ae7899255261f3a6af16010567c1e20907e
beau-haldane <beauhaldane@gmail.com>
2024-02-12 17:48:45 +1000

    Update employment details

--- src/content/employmentDetails.ts
+++ src/content/employmentDetails.ts
@@ -2,26 +2,27 @@ import type { JobDetails } from '../types';
 
 export const employmentHistory: JobDetails[] = [
   {
-    jobTitle: 'Junior Software Engineer',
+    jobTitle: 'Junior Software Developer',
     employer: 'Cadent',
     employerWebsite: 'https://cadent.au/',
     employmentType: 'Full Time, Hybrid',
     tenure: 'Sep 2023 - Jan 2024',
     achievements: [
-      `Full-stack software engineer primarily working with TypeScript, NextJS, Postgres, Supabase, as well as some project work using Dynamics 365 & jQuery`,
+      `Consultancy environment which necessitated balancing multiple projects, allocating time effectively, and tracking time for billing purposes.`,
+      `Full-stack software developer primarily working with TypeScript, NextJS, Postgres, Supabase, as well as Dynamics 365`,
       `Assisted with designing and developing a user management system for a medium sized online publication including a basic BI tool for the management team to gain insights into their users`,
       `Worked on a large government initiative designed to enable skilled workers from Pacific Island nations to gain employment with Australian businesses which struggle to find employees`,
     ],
   },
   {
-    jobTitle: 'Junior Software Engineer',
-    jobTitleSecondary:'& Intern Software Developer (Until Jul 2023)',
+    jobTitle: 'Junior Software Developer',
+    jobTitleSecondary: '& Intern Software Developer (Until Jul 2023)',
     employer: 'Coviu Global',
     employerWebsite: 'https://www.coviu.com',
     employmentType: 'Full Time, Remote',
     tenure: 'Oct 2022 - Sep 2023',
     achievements: [
-      `Full-stack engineer working across the entire codebase from writing data models for the company’s ELT data warehouse, to developing new front-end features with React.`,
+      `Full-stack developer working across the entire codebase from writing data models for the company’s ELT data warehouse, to developing new front-end features with React.`,
       `Developed foundational skills working on large-scale software systems in a multi-team environment, including version control, CI/CD, and Agile methodologies`,
       `Undertook a large back-end project to refactor a number of business reports. Improved design patterns, database querying, type safety, as well as the reliability of the reports for the end user.`
     ],
